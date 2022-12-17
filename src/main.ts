40dfda9f27279bd7b6ff087504e33cac56433efe
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-17 13:13:43 +0100

    ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,34 @@
 import './style.css'
+
+type Config = {
+  headers: Headers
+}
+
+type Headers = {
+  Accept: string
+}
+
+type JSONResponse = {
+  joke?: string
+}
+
+const jokeEl = document.getElementById('joke') as HTMLDivElement
+const jokeBtn = document.getElementById('jokeBtn') as HTMLButtonElement
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke(): Promise<void> {
+  const config: Config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config) as Response
+
+  const data = await res.json() as JSONResponse
+
+  jokeEl.innerHTML = data.joke!
+}
